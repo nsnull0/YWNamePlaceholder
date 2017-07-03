49057daed9bc957d2cd22b7d3ea3695f96a43717
yoseph_wijaya <info@yoseph.ws>
2017-07-03 21:12:58 +0900

    Initial Commit

---
+++ YWNamePlaceholder/YWNamePlaceholder.h
@@ -0,0 +1,19 @@
+//
+//  YWNamePlaceholder.h
+//  YWNamePlaceholder
+//
+//  Created by Yoseph Wijaya on 2017/07/03.
+//  Copyright © 2017 Yoseph Wijaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YWNamePlaceholder.
+FOUNDATION_EXPORT double YWNamePlaceholderVersionNumber;
+
+//! Project version string for YWNamePlaceholder.
+FOUNDATION_EXPORT const unsigned char YWNamePlaceholderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YWNamePlaceholder/PublicHeader.h>
+
+
